f11bd42a544679a40ec9a284d951808f468bb794
BalamuruganDV <102686521+BalamuruganDV@users.noreply.github.com>
2022-05-01 16:18:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/BalamuruganDV/LUNA-NO-FEATURES.git /LUNA-NO-FEATURES
+  git clone https://github.com/BalamuruganDV/LUNA-FILTER.git /LUNA-FILTER
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LUNA-NO-FEATURES
+  git clone $UPSTREAM_REPO /LUNA-FILTER
 fi
-cd /LUNA-NO-FEATURES
+cd /LUNA-FILTER
 pip3 install -U -r requirements.txt
 echo "Starting Luna❤️...."
 python3 bot.py
